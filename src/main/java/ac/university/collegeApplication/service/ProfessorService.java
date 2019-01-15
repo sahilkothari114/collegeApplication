05d77c5ade2c809270a691f5e72560bf2a1b48f6
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-15 23:59:26 +0530

    Bean creation on Department and Professor

--- src/main/java/ac/university/collegeApplication/service/ProfessorService.java
+++ src/main/java/ac/university/collegeApplication/service/ProfessorService.java
@@ -2,7 +2,9 @@ package ac.university.collegeApplication.service;
 
 
 import ac.university.collegeApplication.entity.Professor;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ProfessorService {
     public String add(Professor department);
     public void delete(String id);
