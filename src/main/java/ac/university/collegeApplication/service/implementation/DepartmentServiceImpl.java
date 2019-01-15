34c97dd6a53a91746d6f39df55ec64139cfcc57d
sid <siddharthakhanna19@gmail.com>
2019-01-15 23:04:36 +0530

    File management

--- src/main/java/ac/university/collegeApplication/service/implementation/DepartmentServiceImpl.java
+++ src/main/java/ac/university/collegeApplication/service/implementation/DepartmentServiceImpl.java
@@ -1,6 +1,28 @@
 package ac.university.collegeApplication.service.implementation;
 
+import ac.university.collegeApplication.entity.Department;
 import ac.university.collegeApplication.service.DepartmentService;
 
 public class DepartmentServiceImpl implements DepartmentService {
+
+
+    @Override
+    public String add(Department department) {
+        return null;
+    }
+
+    @Override
+    public String delete(String id) {
+        return null;
+    }
+
+    @Override
+    public String update(Department department) {
+        return null;
+    }
+
+    @Override
+    public Department select(String id) {
+        return null;
+    }
 }
