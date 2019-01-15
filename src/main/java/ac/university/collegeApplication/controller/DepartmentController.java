9e6ceb6cc661e9e899ffd8b1353005d83d86ab0e
sid <siddharthakhanna19@gmail.com>
2019-01-15 22:56:33 +0530

    File management

--- src/main/java/ac/university/collegeApplication/controller/DepartmentController.java
+++ src/main/java/ac/university/collegeApplication/controller/DepartmentController.java
@@ -1,11 +1,31 @@
 package ac.university.collegeApplication.controller;
 
 
+import ac.university.collegeApplication.dto.DepartmentDTO;
+import ac.university.collegeApplication.entity.Department;
+import ac.university.collegeApplication.service.DepartmentService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    DepartmentService departmentService;
+    @RequestMapping("/addDepartment")
+    public String add(@RequestBody DepartmentDTO[] departmentDTO){
+        for (DepartmentDTO d:departmentDTO
+             ) {
+            Department department=new Department();
+            BeanUtils.copyProperties(d,department);
+            departmentService.add(department);
+        }
+
+        return "added";
+    }
 
 
 }
--- src/main/java/ac/university/collegeApplication/service/DepartmentService.java
+++ src/main/java/ac/university/collegeApplication/service/DepartmentService.java
@@ -1,4 +1,11 @@
 package ac.university.collegeApplication.service;
 
+import ac.university.collegeApplication.entity.Department;
+
 public interface DepartmentService {
+    public String add(Department department);
+    public String delete(String id);
+    public String update(Department department);
+    public Department select(String id);
+
 }
