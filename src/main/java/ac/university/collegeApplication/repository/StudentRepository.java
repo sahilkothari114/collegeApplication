875642d89116a6312f63a172802e4d8d56567bd5
vyshnavipulusu <15pa1a05e1@vishnu.edu.in>
2019-01-16 00:28:24 +0530

    Subject

--- src/main/java/ac/university/collegeApplication/repository/StudentRepository.java
+++ src/main/java/ac/university/collegeApplication/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package ac.university.collegeApplication.repository;
 
 import ac.university.collegeApplication.entity.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student,String> {
 }
