01b4f5ec2393ccdffbf6871c323a38bd9e2cb8bd
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-15 23:38:19 +0530

    Changes to Professor

--- src/main/java/ac/university/collegeApplication/controller/ProfessorController.java
+++ src/main/java/ac/university/collegeApplication/controller/ProfessorController.java
@@ -1,4 +1,44 @@
 package ac.university.collegeApplication.controller;
 
+import ac.university.collegeApplication.dto.DepartmentDTO;
+import ac.university.collegeApplication.dto.ProfessorDTO;
+import ac.university.collegeApplication.entity.Department;
+import ac.university.collegeApplication.entity.Professor;
+import ac.university.collegeApplication.service.DepartmentService;
+import ac.university.collegeApplication.service.ProfessorService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProfessorController {
+
+    @Autowired
+    ProfessorService professorService;
+
+    @RequestMapping("/addProfessor")
+    public String add(@RequestBody ProfessorDTO[] professorDTO){
+        for (ProfessorDTO p:professorDTO
+                ) {
+            Professor professor=new Professor();
+            BeanUtils.copyProperties(p,professor);
+            professorService.add(professor);
+        }
+
+        return "added";
+    }
+
+    @GetMapping("/selectProfessor")
+    public Professor selectDepartment(@RequestParam String departmentId)
+    {
+        return professorService.select(departmentId);
+    }
+
+    @DeleteMapping("/deleteProfessor")
+    public void deleteDepartment(@RequestParam String departmentId)
+    {
+        professorService.delete(departmentId);
+    }
+
+
 }
--- src/main/java/ac/university/collegeApplication/service/implementation/ProfessorServiceImpl.java
+++ src/main/java/ac/university/collegeApplication/service/implementation/ProfessorServiceImpl.java
@@ -1,6 +1,36 @@
 package ac.university.collegeApplication.service.implementation;
 
+import ac.university.collegeApplication.entity.Professor;
+import ac.university.collegeApplication.repository.ProfessorRepository;
 import ac.university.collegeApplication.service.ProfessorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ProfessorServiceImpl implements ProfessorService {
+
+    @Autowired
+    ProfessorRepository professorRepository;
+
+    @Override
+    public String add(Professor department) {
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+        professorRepository.delete(id);
+    }
+
+    @Override
+    public String update(Professor department) {
+        return null;
+    }
+
+    @Override
+    public Professor select(String id) {
+        return professorRepository.findOne(id);
+    }
 }
