d3ea2c0b6f2a8ad58095d83af3186ff190da1020
sid <siddharthakhanna19@gmail.com>
2019-01-16 12:47:18 +0530

    File management

--- src/main/java/ac/university/collegeApplication/repository/StudentRepository.java
+++ src/main/java/ac/university/collegeApplication/repository/StudentRepository.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends CrudRepository<Student,String> {
 
 
+    @Query(value="select avg(marks*credit)/(10*sum(credit)) from score inner join subject on score.subjectid=subject.subjectid where studentid= ?1 and semester= ?2",nativeQuery = true)
+    double findAvgMarksCurrentSem(String studentid,String semester);
 
+    @Query(value = "select avg(marks*credit)/(10*sum(credit)) from score inner join subject on score.subjectid=subject.subjectid where studentid= ?1",nativeQuery = true)
+    double findAvgMarks(String studentid);
 
 }
